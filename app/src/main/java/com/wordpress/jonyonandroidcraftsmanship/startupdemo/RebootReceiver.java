5a19b46587b6f0bf928424b6055e64de7be2049a
MdManjurulHoqueJony <kucse03jony@gmail.com>
2016-03-14 23:58:38 +0600

    Initial commit

---
+++ app/src/main/java/com/wordpress/jonyonandroidcraftsmanship/startupdemo/RebootReceiver.java
@@ -0,0 +1,14 @@
+package com.wordpress.jonyonandroidcraftsmanship.startupdemo;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class RebootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent myIntent=new Intent(context,MainActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(myIntent);
+    }
+}
